8c2193fb580648bc6b10a984b32044856b7bffa5
Lee Nieves <lanaisabella.nieves@ucalgary.ca>
2022-04-07 20:25:56 -0600

    Order page now interacts with back-end. Appearance is a bit more consistent with other pages. Order items with >1 quantity have their own entry. Added Side and Special instruction to each entry for differentiation.

--- SpeeDine.Backend/Models/OrderItem.cs
+++ SpeeDine.Backend/Models/OrderItem.cs
@@ -9,6 +9,7 @@ namespace SpeeDine.Backend.Models
     public record OrderItem(int Id, MenuItem MenuItem, int Quantity, OrderStatus OrderStatus, MenuSide? Side, string SpecialInstructions) 
     {
         public int Quantity { get; set; } = Quantity;
+        public OrderStatus OrderStatus { get; set; } = OrderStatus;
     };
 
     public enum OrderStatus
